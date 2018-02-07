3ff87fd3eccfa3e683fbf53bed6ef355da93968e
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-02-07 16:39:03 -0500

    Corrected the name for the TB test results.

--- src/PerformanceTesting/PerformanceTest.java
+++ src/PerformanceTesting/PerformanceTest.java
@@ -11,7 +11,7 @@ import CaseBaseCreation.LogFile2CaseBase;
 public class PerformanceTest extends PerformanceEvaluator {
 	public static void main(String a[]) throws IOException{
 		String [] filenames = {"Data/log_toggle1.txt","Data/log_toggle2.txt","Data/log_toggle3.txt","Data/log_toggle4.txt","Data/log_toggle5.txt","Data/log_toggle6.txt","Data/log_toggle7.txt","Data/log_toggle8.txt","Data/log_toggle9.txt", "Data/log_toggle10.txt"};
-		String output_filename = "Results/ctsmodel,bayesian,none,none,none,none,.csv";
+		String output_filename = "Results/CBR,TB,none,none,none,none,.csv";
 	
 		//CaseBaseFilter smote = new UnderSampling(standardize);
 		//CaseBaseFilter sample = new Sampling(standardize);
